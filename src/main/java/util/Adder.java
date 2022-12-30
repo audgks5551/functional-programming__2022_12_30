2edead18baabffab29f1b3d1b41eb399ddf01a3c
audgks5551 <audgks5551@daum.net>
2022-12-30 15:44:44 +0900

    람다식을 사용하고 줄여보기

--- src/main/java/util/Adder.java
+++ src/main/java/util/Adder.java
@@ -21,5 +21,11 @@ class Main {
         // 너무 불편해...
         // 그냥 메서드로 만드는게 낫지 않나?? static 함수로 만들면 편할텐데..
         // 왜 객체를 함수로 만드는 걸까??
+        // 람다식이 존재하네 한번 사용해보자!!
+
+        Function<Integer, Integer> functionAdder2 = x -> x + 10;
+        int result2 = functionAdder2.apply(5);
+        System.out.println(result2);
+        // 어? 클래스를 만들지 않아도 객체를 함수로 만들 수 있네!!
     }
 }
