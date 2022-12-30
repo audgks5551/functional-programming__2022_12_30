1003b9967926d0c053c50831a5f21e615bde6386
audgks5551 <audgks5551@daum.net>
2022-12-30 19:14:15 +0900

    ClassName::instanceMethodName

--- src/main/java/chapter5/Chapter5Section1.java
+++ src/main/java/chapter5/Chapter5Section1.java
@@ -1,8 +1,13 @@
 package chapter5;
 
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.BiFunction;
 
 public class Chapter5Section1 {
 
@@ -20,6 +25,12 @@ public class Chapter5Section1 {
 
     public void myMethod() {
         System.out.println(calculate(10, 3, this::subtract));
+    }
+
+    public static void printUserField(List<User> users, Function<User, Object> getter) {
+        for (User user : users) {
+            System.out.println(getter.apply(user));
+        }
     }
 
     public static void main(String[] args) {
@@ -44,5 +55,21 @@ public class Chapter5Section1 {
         System.out.println(calculate(8, 2, (x, y) -> instance.subtract(x, y)));
         System.out.println(calculate(8, 2, instance::subtract));
         instance.myMethod();
+
+        Function<String, Integer> functionStrLength = s -> s.length();
+        Function<String, Integer> functionStrLength2 = String::length;
+        System.out.println(functionStrLength2.apply("hello"));
+
+        BiPredicate<String, String> functionStrEquals = (s, anObject) -> s.equals(anObject);
+        BiPredicate<String, String> functionStrEquals2 = String::equals;
+        functionStrEquals.test("hello", "world");
+
+        List<User> users = new ArrayList<>();
+        users.add(new User(3, "mark"));
+        users.add(new User(1, "Charlie"));
+        users.add(new User(5, "Bob"));
+
+        printUserField(users, user -> user.getId());
+        printUserField(users, User::getName);
     }
 }
